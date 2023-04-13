e4ab1858f63283745e0b814d8b43798713186e44
Vimalsri5158 <125827626+Vimalsri5158@users.noreply.github.com>
2023-04-13 11:06:45 +0530

    Add files via upload

---
+++ script .js
@@ -0,0 +1,11 @@
+function foo(){
+
+var form=document.getElementById("Firstname").value;
+var form1=document.getElementById("Middlename").value;
+var form2=document.getElementById("Lastname").value;
+var form3=document.getElementById("Email").value;
+console.log(form);
+console.log(form1);
+console.log(form2);
+console.log(form3);
+}
